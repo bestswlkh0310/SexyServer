0b480ea71c27e8bc47af21ecbbf0a7737e3e005d
이강현 <hhhello0507@gmail.com>
2023-12-07 19:45:25 +0900

    Init :: Project Setting

---
+++ src/main/kotlin/com/bestswlkh0310/sexyserver/SexyServerApplication.kt
@@ -0,0 +1,11 @@
+package com.bestswlkh0310.sexyserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SexyServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SexyServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/bestswlkh0310/sexyserver/SexyServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bestswlkh0310.sexyserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SexyServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
